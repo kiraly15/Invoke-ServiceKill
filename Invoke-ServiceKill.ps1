2e0cb3644276995878125d16988d259757228b3a
s3ct3st <dankiralync@gmail.com>
2017-07-19 10:37:05 -0400

    Add files via upload

---
+++ Invoke-ServiceKill.ps1
@@ -0,0 +1,18 @@
+function Invoke-ServiceKill 
+{
+
+# Get-Service | Sort-Object status,displayname | ft
+# Get-Service | Write-Output
+# Get-Service | Sort-Object status,displayname | ForEach-Object {write-output $_.Displayname "<=|=|=>" $_.status `r`n}
+# Get-Service | Sort-Object status,name,displayname | ForEach-Object {write-output $_.name " || " $_.Displayname " || " " Service Status: " $_.status `r`n}
+# $Service = Read-Host -Prompt 'Please enter the service name you would like to stop...'
+
+# param
+#    (
+#        [Parameter(Mandatory = $True)]
+#        [string]
+#        $Service
+#    )
+Stop-Service CylanceSVC
+}
+
